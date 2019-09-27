87eda68bdcefabdf50a7ade936facb55550c8d43
joaquings <44094396+joaquings@users.noreply.github.com>
2019-09-27 17:19:00 -0300

    up

--- listas.py
+++ listas.py
@@ -1,7 +1,6 @@
 #Listas de respuestas custom para webhook fullfilment.
 
 
-
 fallbackList = ['No te escuché muy bien. ¿Podrías repetirlo?', 
 'Debo estar cansada, no escuché lo que dijiste.', 
 'Respuesta custom de fallback']
